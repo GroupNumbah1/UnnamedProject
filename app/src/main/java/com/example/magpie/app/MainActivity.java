f48e907f4c8d9885d0b24ddf5e960869ffa24076
jbird1620 <jessey.birdrattler@umontana.edu>
2017-02-22 16:57:22 -0700

    Added Start screen to app. with main menu

--- app/src/main/java/com/example/magpie/app/MainActivity.java
+++ app/src/main/java/com/example/magpie/app/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.magpie.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
+        //Button Press
+        SetUpButtonPress();
+    }//Oncreate
+
+    //Function for button press
+    private void SetUpButtonPress(){
+
+        Button  runAppButton = (Button)findViewById(R.id.button10);
+        runAppButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }//SetUpButtonPress
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -33,5 +52,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-}
+    }*/
+}//class
